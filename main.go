9741b527fe1d9e74109d5e085b1bfa78563261eb
pfussell <patrickfussell@gmail.com>
2019-11-15 08:57:02 -0500

    updated all code

--- main.go
+++ main.go
@@ -1,36 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-
-	gn "github.com/tomsteele/go-nmap"
+	"gophermap/parser"
 )
 
 func main() {
-	fl, err := ioutil.ReadFile("test.xml")
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
+	//fl, err := ioutil.ReadFile("test.xml")
+	//if err != nil {
+	//	fmt.Println("Error: ", err)
+	//}
 
-	prettyPrint(fl)
-}
-
-func prettyPrint(f []byte) {
-	n, err := gn.Parse(f)
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	for _, host := range n.Hosts {
-
-		for _, ip := range host.Addresses {
-
-			for _, port := range host.Ports {
-				fmt.Println(host.Hostnames[0].Name, " | ", ip.Addr, " | ", port.PortId)
-
-			}
-
-		}
-	}
+	//prettyPrint(fl)
+	parser.NessusPrettyPrint()
+	
 }
---
+++ parser/parser.go
@@ -0,0 +1,103 @@
+package parser
+
+import (
+	"fmt"
+	"encoding/json"
+	gn "github.com/tomsteele/go-nmap"
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
+// NmapPrettyPrint consumes nmap xml and prints
+// formatted table of enumed services 
+func NmapPrettyPrint(f []byte) {
+	n, err := gn.Parse(f)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	for _, host := range n.Hosts {
+
+		for _, ip := range host.Addresses {
+
+			for _, port := range host.Ports {
+				fmt.Println(host.Hostnames[0].Name, " | ", ip.Addr, " | ", port.PortId)
+
+			}
+
+		}
+	}
+}
+
+// NessusPrettyPrint consumes an nessus csv and 
+// prints out service and IP 
+func NessusPrettyPrint() {
+	// Open the file
+	csvfile, err := os.Open("test.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if record[7] == "Service Detection" {
+			fmt.Printf("IP: %s Port: %s\n", record[4], record[6])
+		}
+	}
+
+}
+
+// RumblePrettyPrint is for parsing
+// Rumble scans in nmap xml format
+func RumblePrettyPrint(f []byte) {
+	n, err := gn.Parse(f)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	for _, host := range n.Hosts {
+				for _, ip := range host.Addresses {
+					fmt.Println("|--------------|-------------------|--------------|------------------|")
+					for _, port := range host.Ports {
+						if port.Service.Product == "" {
+							if len(port.Protocol) == 0 {
+								fmt.Println("| ", ip.Addr, " | ", port.PortId, " | ", "UNKNOWN", " | ", " UNKNOWN ", " | ")
+							} else {
+								m := make(map[string]string)
+								b := []byte(port.Scripts[0].Output)
+								err := json.Unmarshal(b, &m)
+								if err != nil {
+									fmt.Println("Error parsing embedded JSON")
+								} else {
+									if banner, exists := m["banner"]; exists {
+										fmt.Println("*** FOUND BANNER ***")
+										fmt.Println("| ", ip.Addr, " | ", port.PortId, " | ", port.Protocol, " | ", " UNKNOWN ", " | ", banner)
+									}
+								}
+							}
+							fmt.Println("| ", ip.Addr, " | ", port.PortId, " | ", port.Protocol, " | ", " UNKNOWN ", " | ")
+						} else {
+							if len(port.Protocol) == 0 {
+								fmt.Println("| ", ip.Addr, " | ", port.PortId, " | ", " UNKNOWN ", " | ", port.Service.Product, port.Service.Version, " | ", port.Scripts[0].Output)
+							} else {
+								fmt.Println("| ", ip.Addr, " | ", port.PortId, " | ", port.Protocol, " | ", port.Service.Product, port.Service.Version, " | ", port.Scripts[0].Output)
+							}
+						}
+					}
+				}
+			}
+}
